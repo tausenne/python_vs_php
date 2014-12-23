3a86485cdd7cd1bd9f2e930b12350d3d593064a6
T S <tausenne@gmail.com>
2014-12-23 11:00:01 -0500

    cleaning up

--- abracadabra.py
+++ abracadabra.py
@@ -4,7 +4,7 @@ import sys
 start = time.clock()
 
 top_cap = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-print 'Running the searcher over ' + str(top_cap) + ' iterations'
+print 'Running the searcher over %d iterations' % top_cap
 
 for cycle in range(0, top_cap):
     count_found = total_lines = 0
@@ -12,7 +12,7 @@ for cycle in range(0, top_cap):
         for line in fs:
             total_lines += 1
             if line.find('abracadabra') > -1:
-                count_found += 1 # there're no ++ and -- operators in python
+                count_found += 1
 
 plural = ''
 if count_found == 0:
@@ -20,14 +20,14 @@ if count_found == 0:
 elif count_found == 1:
     verb = 'was a total of 1'
 else:
-    verb = 'were a total of ' + str(count_found)
+    verb = 'were a total of %d' % count_found
     plural = 's'
 
-print 'There ' + verb + ' abracadabra' + plural + ' in the file'
+print 'There %s abracadabra %s in the file' % (verb, plural)
 
-print 'There were a total of ' + str(total_lines) + ' lines to evaluate'
+print 'There were a total of %d lines to evaluate' % total_lines
 
 end = time.clock()
 diff = end - start
 
-print 'The whole process took ' + str(diff) + ' s'
+print 'The whole process took %f s' % diff
